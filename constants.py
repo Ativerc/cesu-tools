738e6b280544b3e64a43da98795bcbaa5c694a88
Pratyush Mohapatra <ppmvcs@gmail.com>
2020-06-01 13:24:08 +0530

    Revamped URL variables in constants.py CHANGED: constants.py * constants.py 	* Added is_portal_up() to add functionality in future to check which of the CESU portals are up. 		* This returns the working cesu portal to  CESU_WORKING_PORTAL 	* Renamed and changed the values of URL variables to help make it easier to code. 		* eg. LOGIN_PORTAL_URL is now equals to CESU_WORKING_PORTAL + "/ConsumerPortal/welcome.jsp" 		* previously it was just equal to "/ConsumerPortal/welcome.jsp" 		* Which meant I had to set the working CESU portal + the string as the url in the main code

--- constants.py
+++ constants.py
@@ -3,14 +3,24 @@ from pathlib import Path
 
 
 
-CESU_CONSUMER_PORTAL_1 = "117.239.112.120:8070"
-CESU_CONSUMER_PORTAL_2 = "203.193.144.25:8080"
+CESU_CONSUMER_PORTAL_1 = "http://117.239.112.120:8070"
+CESU_CONSUMER_PORTAL_2 = "http://203.193.144.25:8080"
 
-LOGIN_PAGE = "/ConsumerPortal/welcome.jsp"
-LOGIN_URL = "/ConsumerPortal/Record_Check?strConsID=USERNAME&OptType=Login&strDivCode=0&strConsumerType=0&strConsNo=&txtInput=LOLTCHA&image2.x=73&image2.y=14"
-BILLS_PAGE_URL = "/ConsumerPortal/bill_s4.jsp"
-DETAILED_BILL_URL = "/ConsumerPortal/bill_det.jsp?DIVCODE=DC&CONS_ACC=USERNAME&CESU_DATE=DD-MM-YYYY"
-SBM_BILL_URL = "/ConsumerPortal/SBM_Bill_Format.jsp?DIVCODE=DC&CONS_ACC=USERNAME&CESU_DATE=DD-MM-YYYY"
+def is_portal_up():
+    # Code to check when/which one of the portals(1or2) of the CESU is up:
+    # This is hard to check because CESU won't turn up an error. 
+    # But returns 200 and shows an error page while the entire backend is down
+    # this happens so less that, its hard for me to reproduce this error in order to debug and write code for it.
+    # for now this code returns CESU_CONSUMER_PORTAL_1 
+    return CESU_CONSUMER_PORTAL_1
+
+CESU_WORKING_PORTAL = is_portal_up()
+
+LOGIN_PAGE_URL = CESU_WORKING_PORTAL + "/ConsumerPortal/welcome.jsp"
+LOGIN_URL = CESU_WORKING_PORTAL + "/ConsumerPortal/Record_Check"
+BILLS_PAGE_URL = CESU_WORKING_PORTAL + "/ConsumerPortal/bill_s4.jsp"
+DETAILED_BILL_URL = CESU_WORKING_PORTAL + "/ConsumerPortal/bill_det.jsp"
+SBM_BILL_URL = CESU_WORKING_PORTAL + "/ConsumerPortal/SBM_Bill_Format.jsp"
 
 # FILEPATHS
 CESU_TOOLS_DIRECTORY_PATH = ""
